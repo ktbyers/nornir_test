4bdd3bd9ca3999963db23e757eef833871b8e5cd
Kirk Byers <ktbyers@twb-tech.com>
2018-07-26 15:30:56 -0700

    More napalm examples

--- nornir_test/napalm_examples/case5_napalm_multi/napalm_get_x.py
+++ nornir_test/napalm_examples/case5_napalm_multi/napalm_get_x.py
@@ -1,7 +1,7 @@
 from nornir.core import InitNornir
 from nornir.plugins.tasks.networking import napalm_get
 
-from pprint import pprint
+from nornir_test.nornir_utilities import nornir_set_creds, std_print
 
 # Turn off self-signed cert warnings
 import requests
@@ -9,20 +9,10 @@ from requests.packages.urllib3.exceptions import InsecureRequestWarning
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
 
-def std_print(agg_result):
-    print()
-    for k, multi_result in agg_result.items():
-        print('-' * 50)
-        print(k)
-        for result_obj in multi_result:
-            pprint(result_obj.result)
-        print('-' * 50)
-        print()
-    print()
-
-
 def main():
     brg = InitNornir(config_file="./nornir.yml")
+    nornir_set_creds(brg)
+
     result = brg.run(
         task=napalm_get,
         getters=["facts"],
