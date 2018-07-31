45f665950e755c87fc9346b8bb97a8d76bea6ffa
Kirk Byers <ktbyers@twb-tech.com>
2018-07-31 10:40:48 -0700

    Updates to reference nornir examples

--- nornir_test/napalm_examples/case6_napalm_xr/napalm_get_x.py
+++ nornir_test/napalm_examples/case6_napalm_xr/napalm_get_x.py
@@ -3,11 +3,6 @@ from nornir.plugins.tasks.networking import napalm_get
 
 from nornir_test.nornir_utilities import nornir_set_creds, std_print
 
-# Turn off self-signed cert warnings
-import requests
-from requests.packages.urllib3.exceptions import InsecureRequestWarning
-requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
-
 
 def main():
     brg = InitNornir(config_file="./nornir.yml")
